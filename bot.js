744c8fabc929a50f41a5eb32f4722e67c44c7673
zombiie88 <44290773+zombiie88@users.noreply.github.com>
2018-10-19 15:47:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,23 @@ client.on('message', message => {
 });
 
 
+
+var prefix = "!";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
